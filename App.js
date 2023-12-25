53d8fddd057af1cc05795ae0b7311991e46b91c1
Abhishek <abhishek.tanwar@cloverventures.in>
2023-12-26 04:31:43 +0530

    Integrate reactjs to a program in vanilla HTML, JS

---
+++ App.js
@@ -0,0 +1,11 @@
+const parent = React.createElement(
+  'div',
+  {id:"parent"},
+  [
+    React.createElement('h1',{},"Hi I am h1 tag!"),
+    React.createElement('h2',{},"Hi I am h2 tag!")
+  ]
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
